c784f38f3c56329d1543c554b068d1283b355e9d
IndicVision <155136269+IndicVision@users.noreply.github.com>
2024-10-09 16:55:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+// DOM
+const url = "http://localhost:5000"
+const chatBox = document.getElementById('chats');
+var messages = [{
+    "role":"system",
+    "content":`You are an assistant for question-answering tasks.
+    Use the attached information to answer the question.
+    If asked for a graph, generate python code that will draw the graph (in size (6,8))`
+}];
+
+const inputField = document.getElementById('userInput');
+inputField.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+});
+
+if (chatBox.childNodes.length === 3) {
+    instructionMsgDiv = document.createElement('div');
+    instructionMsgDiv.classList.add('instruct-msg');
+    instructionMsgDiv.innerHTML = `
+        <h2 style="text-align: center;">PowerBot</h2>
+    `;
+    chatBox.appendChild(instructionMsgDiv);
+}
+
+// API Calls
+async function sendMessage() {
+    const userInput = document.getElementById("userInput").value;
+    if (instructionMsgDiv) {
+        chatBox.removeChild(instructionMsgDiv);
+        instructionMsgDiv = null;
+    }
+    if (userInput.trim() !== "") {
+        const userMessageDiv = document.createElement('div')
+        userMessageDiv.classList.add('message');
+        userMessageDiv.textContent = userInput;
+        chatBox.appendChild(userMessageDiv)
+        inputField.value = "";
+
+        messages.push({
+            "role":"user",
+            "content":[{"type":"text", "text":userInput}]
+        })
+        await fetch(url+"/chat", {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                query: userInput,
+                messages: messages,
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            const responseDiv = document.createElement("div");
+            responseDiv.classList.add("response");
+            console.log(data)
+            if (data.image) {
+                responseDiv.innerHTML = `
+                    <img src="images/logo.png" id="logo">
+                    ${marked.parse(data.response)}
+                    <img style="max-width: 400px;"src="data:image/png;base64, ${data.image}">`;
+            } else {
+                responseDiv.innerHTML = `
+                    <img src="images/logo.png" id="logo">
+                    ${marked.parse(data.response)}`;
+            }
+            console.log(data.messages)
+            messages = data.messages
+            messages.push({
+                "role":"assistant",
+                "content":data.response
+            })
+            chatBox.appendChild(responseDiv);
+            chatBox.scrollTop = chatBox.scrollHeight;
+        })
+        .catch(error => {
+            console.error("Error: ", error);
+        })
+    }
+}
